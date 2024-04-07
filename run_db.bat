257771c946bfd0bf89c4f68a5af8e79e13b33bb4
Dawid Roszman <dawidroszman@gmail.com>
2024-04-07 21:04:32 +0200

    Minor change to scripts add --rm as qol feature

--- run_db.bat
+++ run_db.bat
@@ -1,1 +1,1 @@
-docker run --name mongodb_wip -p 27017:27017 -v C:\mongodb\wip-be-data:/data/db mongo:latest
+docker run --rm --name mongodb_wip -p 27017:27017 -v C:\mongodb\wip-be-data:/data/db mongo:latest
--- run_db.sh
+++ run_db.sh
@@ -1,1 +1,1 @@
-docker run --name mongodb_wip -p 27017:27017 -v $HOME/mongodb/wip-be-data:/data/db mongo:latest
+docker run --rm --name mongodb_wip -p 27017:27017 -v $HOME/mongodb/wip-be-data:/data/db mongo:latest
