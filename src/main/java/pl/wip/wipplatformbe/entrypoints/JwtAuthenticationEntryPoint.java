054556618f9945aa6d206061181bb6e061bdf50d
Wikator <wiktor@szymulewicz.com>
2024-04-21 16:47:13 +0200

    feature(86byb8er1): added json body when unauthorized

--- src/main/java/pl/wip/wipplatformbe/entrypoints/JwtAuthenticationEntryPoint.java
+++ src/main/java/pl/wip/wipplatformbe/entrypoints/JwtAuthenticationEntryPoint.java
@@ -14,8 +14,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) throws IOException {
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"Access Denied: Authentication required.\"}");
     }
 }
