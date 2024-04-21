b4c0ff9fd59a2bde0b8806564902c226e7bad6b5
Wikator <wiktor@szymulewicz.com>
2024-04-21 15:35:06 +0200

    fix(86byb8er1): setting user as unauthorized when invalid token is provided, rather than returning 200

--- src/main/java/pl/wip/wipplatformbe/middleware/JwtAuthenticationFilter.java
+++ src/main/java/pl/wip/wipplatformbe/middleware/JwtAuthenticationFilter.java
@@ -72,7 +72,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             filterChain.doFilter(request, response);
         } catch (Exception exception) {
-            handlerExceptionResolver.resolveException(request, response, null, exception);
+            filterChain.doFilter(request, response); // TODO: Better error handling
         }
     }
 }
